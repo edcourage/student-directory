3fdb43c5c65427a6e798bce3a395f0fd40b10b1d
Ed Courage <edcourage@live.com>
2019-10-07 17:15:09 +0100

    interative menu amend

--- directory.rb
+++ directory.rb
@@ -1,30 +1,4 @@
 
-def interative_menu
-  students = []
-  loop do
-    puts "1. Input the students"
-    puts "2. Show the students"
-    puts "9. Exit"
-
-    selection = gets.chomp
-
-    case selection
-    when "1"
-      students = input_students
-    when "2"
-      print_header
-      print(students)
-      print_footer(students)
-    when "3"
-      exit
-    else
-      puts "I don't know what you meant, try again"
-    end
-  end
-end
-
-
-
 
 def input_students
   puts "Please enter the names of the students"
